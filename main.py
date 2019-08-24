8af8cc8c26e7770fdca045e788f30e03b9f373cd
Raivat Shah <raivat@u.nus.edu>
2019-08-24 10:17:33 +0800

    Added code to check for tick mark

--- main.py
+++ main.py
@@ -386,7 +386,7 @@ def attendance_reflection(update, context):
         week_counter = 2
         for i in range(66, 78):
             col = chr(i)
-            if wks1.acell(f'{col}{row_num}').value == 1:
+            if wks1.acell(f'{col}{row_num}').value == u"\u2713":
                 weeks.append("Week " + str(week_counter))
                 week_counter += 1
         context.bot.send_message(chat_id=update.message.chat_id,
