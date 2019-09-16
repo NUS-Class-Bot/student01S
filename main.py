6867183cc4bed7aeae5b4ca79f28ed9c7198e6f4
Raivat Shah <raivat@u.nus.edu>
2019-09-16 13:16:56 +0800

    Added api refresh in /comment

--- main.py
+++ main.py
@@ -463,7 +463,10 @@ def comment(update, context):
         context.bot.send_message(chat_id=update.message.chat_id,
                                  text="Sorry! You're not registered as an avenger and hence cannot use this command.")
         return ConversationHandler.END
-
+    global gc
+    global credentials
+    if credentials.access_token_expired:
+        gc.login()
     token = redis_client.hget(AVENGER_MAP, username)
     if token == "No":
         context.bot.send_message(chat_id=update.message.chat_id,
