9bfbbc5124d965688d418ec82c1798e461f0aed8
Raivat Shah <hiraivat@gmail.com>
2020-08-19 12:05:54 +0530

    change week ref default value

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_week_ref():
             print("end" + str(end))
             if start <= date <= end:
                 return data[month][date_range]
-    return 'B'
+    return 'Z'
 
 
 # Enable logging
